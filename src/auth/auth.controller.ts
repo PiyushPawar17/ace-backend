045e223b154c00ec12c5c20000db4487be587d9e
Piyush Pawar <piyushpawar25@gmail.com>
2023-08-27 18:27:10 +0530

    :sparkles: chore: add cookie route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 import { GoogleAuthGuard } from './utils/Guards';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -24,6 +25,18 @@ export class AuthController {
 		return res.redirect(process.env.LOGIN_REDIRECT_URL);
 	}
 
+	@Get('/cookies')
+	@UseGuards(AuthGuard)
+	setCookies(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+		res.cookie('userId', req.user.id, {
+			domain: process.env.CLIENT_ORIGIN,
+			httpOnly: true,
+			secure: true,
+			sameSite: 'none'
+		});
+
+		return res.end();
+	}
 	@Get('logout')
 	logout(@Req() req: Request, @Res() res: Response) {
 		req.logOut({ keepSessionInfo: false }, () => {
